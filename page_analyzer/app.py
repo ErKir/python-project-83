26f400f20b1302bdfecd6945ef5bed96b2e21a32
ErKir <kirillerohin@gmail.com>
2023-08-03 17:01:13 +0300

    feat: add template

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
