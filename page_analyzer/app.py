5b32daf5a2054089af41dbcb8357b335451582c6
ErKir <kirillerohin@gmail.com>
2023-09-05 19:39:39 +0300

    feat: connect db, realized forms and rendering

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -20,7 +20,7 @@ from urllib.parse import urlparse, urlunsplit
 import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.urandom(12)
 
 
 @app.route("/")
@@ -32,24 +32,36 @@ config = dotenv_values("env_vars.env")
 DATABASE_URL = config['DATABASE_URL']
 
 
+# url add
 @app.post("/urls")
 def url():
-    errors = []
+
+    message = {
+        'message': 'Страница успешно добавлена',
+        'type': 'alert-success',
+    }
     data = request.form.to_dict()
     url_obj = urlparse(data['url'])
     cleaned_url = urlunsplit((url_obj.scheme, url_obj.netloc, '', '', '',))
 
-    print('given url is  - ', cleaned_url)
     valid_url = validators.url(cleaned_url)
     valid_len_url = validators.length(cleaned_url, max=255)
+
     if (valid_len_url and valid_url):
         conn = psycopg2.connect(DATABASE_URL)
-        date_time = datetime.datetime.now()
+        date_time = datetime.datetime.now().strftime("%Y-%m-%d")
         with conn.cursor() as db:
-            db.execute(
-                'INSERT INTO urls (name, created_at) VALUES (%s, %s)',
-                (cleaned_url, date_time)
-            )
+            try:
+                db.execute(
+                    'INSERT INTO urls (name, created_at) VALUES (%s, %s)',
+                    (cleaned_url, date_time)
+                )
+                conn.commit()
+            except psycopg2.errors.UniqueViolation:
+                message['message'] = 'Страница уже существует'
+                message['type'] = 'alert-info'
+                conn.rollback()
+
         with conn.cursor() as data:
             data.execute(
                 'SELECT id, name, created_at FROM urls WHERE name=%s',
@@ -57,32 +69,32 @@ def url():
             )
             curr_url = data.fetchall()[0]
             id = curr_url[0]
-            # name = curr_url[1]
-            # created_at = curr_url[2].strftime("%Y-%m-%d")
-            # dic['id'] = id
-            # dic['name'] = name
-            # # 2023-08-07
-            # dic['created_at'] = created_at
-            # print('dic_url==', dic)
-            # flash('Страница успешно добавлена', 'success')
         conn.close()
-        return make_response(redirect(url_for('get_curr_url', id=id)))
-
-    errors = valid_url if valid_len_url is True else valid_len_url
+        flash(message['message'], category=message['type'])
+        resp = make_response(redirect(url_for('get_curr_url', id=id)))
+        resp.headers['X-ID'] = id
+        return resp
+# нет флеша, нужно вроде бы делать рендер темплейт
+    message['message'] = 'Некорректный URL'
+    message['type'] = 'alert-danger'
+    flash(message['message'], category=message['type'])
     return render_template(
         'urls.html',
-        errors=errors
+        message=message,
     )
 
 
 # выводит список сайтов на "/urls"
 @app.get('/urls')
 def urls_get():
+
     conn = psycopg2.connect(DATABASE_URL)
     messages = get_flashed_messages(with_categories=True)
+    print('messages from urls GET- ', messages)
     with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as data:
         data.execute('SELECT * FROM urls')
-        urls = data.fetchall()
+        answer = data.fetchall()
+        urls = [dict(row) for row in answer]
     return render_template(
         'urls.html',
         urls=urls,
@@ -92,47 +104,31 @@ def urls_get():
 
 @app.route("/urls/<id>")
 def get_curr_url(id):
-    print('hi from urls get! looser! id=', id)
-
+    message = get_flashed_messages(with_categories=True)
+    print('messages from get_curr_url- ', message)
     conn = psycopg2.connect(DATABASE_URL)
     with conn.cursor() as data:
         data.execute(
-            'SELECT * FROM urls'
-            # 'SELECT id, name, created_at FROM urls WHERE id=%s',
-            # (str(id),)
+            'SELECT id, name, created_at FROM urls WHERE id=%s',
+            (str(id),)
         )
         curr_url = data.fetchone()
-        print('curr_url==', curr_url)
         dic = {}
         name = curr_url[1]
-        created_at = curr_url[2].strftime("%Y-%m-%d")
+        created_at = curr_url[2]
         dic['id'] = id
         dic['name'] = name
         dic['created_at'] = created_at
     return render_template(
         'urls_add.html',
+        message=message,
         id=dic['id'],
         name=dic['name'],
         created_at=dic['created_at']
     )
 
 
-# Страница успешно добавлена
+# Страница успешно добавлена <div class="alert alert-success" role="alert">Страница успешно добавлена</div>
+# Страница уже существует <div class="alert alert-info" role="alert">Страница уже существует</div>
+# Некорректный URL   <div class="alert alert-danger" role="alert">Некорректный URL</div>
 # 2023-08-07
-
-# @app.post('/posts')
-# def posts_post():
-#     repo = PostsRepository()
-#     data = request.form.to_dict()
-#     errors = validate(data)
-#     if errors:
-#         return render_template(
-#             'posts/new.html',
-#             post=data,
-#             errors=errors,
-#             ), 422
-#     id = repo.save(data)
-#     flash('Post has been created', 'success')
-#     resp = make_response(redirect(url_for('posts_get')))
-#     resp.headers['X-ID'] = id
-#     return resp
