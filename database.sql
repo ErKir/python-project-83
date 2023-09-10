3e72fa8c3cf8d1d94725229ce610edb270451511
ErKir <kirillerohin@gmail.com>
2023-09-10 16:23:36 +0300

    feat: create table url_checks

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
     name       varchar(255) UNIQUE NOT NULL, 
     created_at timestamp NOT NULL
 );
+
+CREATE TABLE url_checks (
+    id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id      bigint,
+    status_code varchar(255),
+    h1          varchar(255),
+    title       varchar(255),
+    description varchar(65535),
+    created_at timestamp NOT NULL
+);
