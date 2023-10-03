921b926dac031717b372d545587023b008839f0b
ErKir <kirillerohin@gmail.com>
2023-10-03 19:17:21 +0300

    feat: add 404 & 500 pages

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -102,3 +102,13 @@ def make_check(id):
     resp = make_response(redirect(url_for('get_curr_url', id=id)))
     resp.headers['X-ID'] = id
     return resp
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
